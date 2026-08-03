03b3a13c869bac036ddc6259e01abc008e3b9b89
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2026-08-03 16:51:51 +0600

    complete

--- src/modules/rental/rental.routes.ts
+++ src/modules/rental/rental.routes.ts
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/', auth('TENANT'), RentalController.submitRequest);
 router.get('/', auth('TENANT', 'LANDLORD'), RentalController.getUserRequests);
+
 router.patch('/landlord/:id', auth('LANDLORD'), RentalController.updateStatus);
 
 export const RentalRoutes = router;
